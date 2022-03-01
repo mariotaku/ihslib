a9a5bc29e678c30e3b4ba8fb676b307ac85914b4
Mariotaku <mariotaku.lee@gmail.com>
2022-03-01 21:34:36 +0900

    Added source file header

--- .idea/fileTemplates/includes/C File Header.h
+++ .idea/fileTemplates/includes/C File Header.h
@@ -1,40 +1,27 @@
+#if ($HEADER_COMMENTS)
 /*
- *  _____  _   _  _____  _  _  _
- * |_   _|| | | |/  ___|| |(_)| |     Steam
+ *  _____  _   _  _____  _  _  _     
+ * |_   _|| | | |/  ___|| |(_)| |     Steam    
  *   | |  | |_| |\ `--. | | _ | |__     In-Home
  *   | |  |  _  | `--. \| || || '_ \      Streaming
  *  _| |_ | | | |/\__/ /| || || |_) |       Library
  *  \___/ \_| |_/\____/ |_||_||_.__/
  *
- * Copyright (c) 2022 Ningyuan Li <https://github.com/mariotaku>.
- *
+ * Copyright (c) $YEAR Ningyuan Li <https://github.com/mariotaku>.
+ * 
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
- *
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
  */
+#end
 
-#pragma once
-
-#include "common.h"
-
-typedef struct IHS_Session IHS_Session;
-
-typedef struct IHS_SessionConfig {
-
-} IHS_SessionConfig;
-
-IHS_Session *IHS_SessionCreate(const IHS_ClientConfig *config);
-
-void IHS_SessionStart(IHS_Session *session, const IHS_SessionConfig *config);
-
-void IHS_SessionDestroy(IHS_Session *session);
--- src/base.h
+++ src/base.h
@@ -1,5 +1,29 @@
-#pragma once
+/*
+ *  _____  _   _  _____  _  _  _
+ * |_   _|| | | |/  ___|| |(_)| |     Steam
+ *   | |  | |_| |\ `--. | | _ | |__     In-Home
+ *   | |  |  _  | `--. \| || || '_ \      Streaming
+ *  _| |_ | | | |/\__/ /| || || |_) |       Library
+ *  \___/ \_| |_/\____/ |_||_||_.__/
+ *
+ * Copyright (c) 2022 Ningyuan Li <https://github.com/mariotaku>.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
 
+#pragma once
 
 #include <stdint.h>
 #include <uv.h>
