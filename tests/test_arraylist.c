5cd6dd8038c8330a63355f1326530a3cc8e2ba45
Ningyuan Li <mariotaku.lee@gmail.com>
2022-12-19 00:05:16 +0900

    updated arraylist test

--- tests/test_arraylist.c
+++ tests/test_arraylist.c
@@ -28,11 +28,13 @@
 
 static int IntFindFn(const void *value, const void *item);
 
-int main(int argc, char *argv[]) {
+int main() {
     IHS_ArrayList list;
     IHS_ArrayListInit(&list, sizeof(int));
 
-    for (int item = 1; item < 50; item += 2) {
+    (*((int *) IHS_ArrayListAppend(&list, NULL))) = 1;
+
+    for (int item = 3; item < 50; item += 2) {
         IHS_ArrayListAppend(&list, &item);
     }
 
@@ -48,16 +50,38 @@ int main(int argc, char *argv[]) {
     index = IHS_ArrayListLinearSearch(&list, &value, IntFindFn);
     assert(index == 3);
 
+    value = 2;
+    index = IHS_ArrayListLinearSearch(&list, &value, IntFindFn);
+    assert(index == -1);
+
     value = 0;
     // Nothing should be removed
     assert(!IHS_ArrayListRemoveFirst(&list, &value));
 
     value = 1;
-    // The reset item should be removed, causes the size to become 24 and the reset item to become 3
+    // The appendedItem item should be removed, causes the size to become 24 and the appendedItem item to become 3
     assert(IHS_ArrayListRemoveFirst(&list, &value));
 
     assert(list.size == 24);
     assert(*((int *) IHS_ArrayListGet(&list, 0)) == 3);
+
+    // Remove the second item (5), so the second item should be 7
+    IHS_ArrayListRemove(&list, 1);
+
+    assert(list.size == 23);
+    assert(*((int *) IHS_ArrayListGet(&list, 1)) == 7);
+
+    // Remove the last item (49)
+    IHS_ArrayListRemove(&list, 22);
+
+    assert(list.size == 22);
+    assert(*((int *) IHS_ArrayListGet(&list, 21)) == 47);
+
+    // Attempt to remove an out of bound item should return false
+    assert(!IHS_ArrayListRemove(&list, 23));
+
+    IHS_ArrayListClear(&list);
+    assert(list.size == 0);
 
     IHS_ArrayListDeinit(&list);
     return 0;
